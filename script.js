184687c9017705acf2aa8c5b523c341d08e9e859
Tushar Verma <110741618+1tushr@users.noreply.github.com>
2023-04-25 20:30:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function(event) {
+  var para = document.createElement("p");
+  var node = document.createTextNode("DOM load success");
+  para.appendChild(node);
+  var element = document.getElementById("div1");
+  element.appendChild(para);
+});
